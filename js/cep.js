f6431fdf3da6827bdf1cce38c7c5fd2d63bd527a
ShinigamiRodrigo <alesoares.2018@hotmail.com>
2018-09-07 17:02:41 -0300

    Formulário Provavelmente Terminado

--- js/cep.js
+++ js/cep.js
@@ -1,32 +1,4 @@
-//Mascara as informações, deixando elas editadas e bonitinhas
-$(document).ready(function(){
-  $('.date').mask('00/00/0000');
-  $('.cep').mask('00000-000');
-  $('.telefone').mask('(00) 0000-0000');
-  $('.cpf').mask('000.000.000-00', {reverse: true});
-
-});
-
-//Impede que Números sejam digitados
-function bloqueiaNumero(texto){
-  var tecla = texto.which || texto.keyCode;
-  if ((tecla >= 65 && tecla <= 90) || (tecla >=97 && tecla <=122) || (tecla == 199 || tecla == 231)){
-    return true;
-  }else{
-    return false;
-  }
-}
-//Impede que Letras sejam digitadas
-function bloqueiaLetra(texto){
-  var tecla = texto.which || texto.keyCode;
-  if ((tecla >=48 && tecla <= 57)){
-    return true;
-  }else{
-    return false;
-  }
-}
-
-/*$("#cep").focusout(function(){
+	$('#cep')[0].focusout(function(){
 		//Início do Comando AJAX
 		$.ajax({
 			//O campo URL diz o caminho de onde virá os dados
@@ -52,4 +24,4 @@ function bloqueiaLetra(texto){
 				$("#numero").focus();
 			}
 		});
-	});*/
+	});
